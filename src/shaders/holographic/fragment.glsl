84e542d39e53a6040b0186acc0948158eb567b77
Johnnie Gonzalez <johnnie.agonz71@gmail.com>
2024-08-23 11:56:23 -0400

    applied faded effect

--- src/shaders/holographic/fragment.glsl
+++ src/shaders/holographic/fragment.glsl
@@ -12,14 +12,19 @@ void main() {
   vec3 normal = normalize(vNormal);
   if(!gl_FrontFacing)
       normal += 1.0;
+
   // Fresnel
   vec3 viewDirection = normalize(vPosition - cameraPosition);
   float fresnel = dot(viewDirection, normal) + 1.0;
   fresnel = pow(fresnel, 2.0);
 
+  // Falloff
+  float falloff = smoothstep(0.8, 0.0, fresnel);
+
   // Holographic
   float holographic = stripes * fresnel;
   holographic += fresnel * 1.25;
+  holographic *= falloff;
 
   // Final color
   gl_FragColor = vec4(1.0, 1.0, 1.0, holographic);
